a798b92cb7e3917d56a7722b55119506d4f6a5b3
muhammetatillaaltan-crypto <muhammetcanaltan@icloud.com>
2026-07-11 17:27:10 +0300

    Initialize Discord bot with commands and extensions
    Set up a Discord bot with command handling and extension loading.

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
+import discord
+from discord.ext import commands
+import asyncio
+import os
 
+intents = discord.Intents.all()
+
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
+
+@bot.event
+async def on_ready():
+    print("=" * 40)
+    print(f"Bot giriş yaptı")
+    print(bot.user)
+    print("=" * 40)
+
+    try:
+        synced = await bot.tree.sync()
+        print(f"{len(synced)} Slash komutu senkronize edildi.")
+    except Exception as e:
+        print(e)
+
+async def load_extensions():
+    for file in os.listdir("./cogs"):
+        if file.endswith(".py"):
+            await bot.load_extension(f"cogs.{file[:-3]}")
+            print(f"{file} yüklendi.")
+
+async def main():
+    async with bot:
+        await load_extensions()
+        from config import TOKEN
+        await bot.start(TOKEN)
+
+asyncio.run(main())
